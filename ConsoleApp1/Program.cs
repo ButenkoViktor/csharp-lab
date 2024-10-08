26fdd88281f19db239c04230ab9ce27781ba64d1
viktor.butenko <viktor.butenko@CH-16>
2024-10-08 17:39:47 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿string message = "";
+message += "Hello";
+message += ",";
+message += " ";
+message += "World!";
+Console.WriteLine(message);
+Console.WriteLine("Przejście do\nnowej linii.");
+Console.WriteLine("tabulacja\tpozioma. ");
+Console.WriteLine("Tabulacja\npionowa. ");
+Console.WriteLine("Alarm!\a");
+Console.WriteLine("Pojedynczy apostrof: \'");
+Console.WriteLine("Podejczy cudzysłów: \"");
+Console.WriteLine("Znak pusty: \0");
